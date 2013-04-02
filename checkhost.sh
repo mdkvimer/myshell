58042165ec4f4b3a3388feb42dc65274868bc1da
mdk <mengdaikun@gmail.com>
2013-04-02 16:00:38 +0800

    Update checkhost.sh

--- checkhost.sh
+++ checkhost.sh
@@ -7,6 +7,6 @@ if [ $HTTP_CODE = 200 ]
 then
 	echo "$URL is Ok" | mail -s "Http_check" root@localhost
 else
-	/usr/local/fetion/fetion  --sid=418860718 --pwd=5566mdk --to=phone number --msg-utf8="$URL is Error;Error  code is $HTTP_CODE"
+	/usr/local/fetion/fetion  --sid=phone id --pwd=5566mdk --to=phone number --msg-utf8="$URL is Error;Error  code is $HTTP_CODE"
 fi
 done
